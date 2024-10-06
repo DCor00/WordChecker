6edaac655359d1ec55b6c8b54bcef8b519fb4edd
DCor00 <diegocorna00@gmail.com>
2024-10-06 12:02:13 +0200

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -10,10 +10,19 @@ typedef struct node {
 
 typedef Node* NodePtr;
 
+// Function to insert a node into a binary search tree (BST)
 void insert(NodePtr* root, NodePtr insertion);
+
+// Function to search for a key in a binary search tree (BST)
 NodePtr search(NodePtr root, char* key, int k);
+
+// Function to delete the entire binary search tree
 void deleteTree(NodePtr root);
+
+// Function to insert a node into a BST
 void insertRB(NodePtr* root, NodePtr rbNode, char* keys, int k, int mall, char* command);
+
+// Function to print all keys in the binary search tree in-order
 void printTree(NodePtr root);
 
 #endif // BST_H
